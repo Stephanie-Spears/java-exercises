ab6d416d00fde8b778e06344c24d4e50cb416725
Stephanie Spears <Stephanie.Spears1@gmail.com>
2017-08-02 00:44:56 -0700

    first io

---
+++ src/exercises/First_IO.java
@@ -0,0 +1,14 @@
+package exercises;
+import java.util.Scanner;
+
+public class First_IO {
+
+    public static void main(String[] args) {
+        Scanner userInput = new Scanner(System.in);
+        System.out.println("Whazzur Name?? Type dat sheeet hurr: ");
+        String in = userInput.nextLine();
+        System.out.println("HEY, " + in + ", Yo name DUMB, DUMMY!" );
+
+
+    };
+}
