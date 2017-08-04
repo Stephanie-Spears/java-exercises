826ac141ad1591da9021445442c3090784d3156d
Stephanie Spears <Stephanie.Spears1@gmail.com>
2017-08-03 17:21:58 -0700

    finish class 2 prep

--- src/exercises/Second_HashMapStuents.java
+++ src/exercises/Second_HashMapStuents.java
@@ -7,7 +7,7 @@ import java.util.Scanner;
 public class Second_HashMapStuents {
     public static void main(String[] args) {
 
-        HashMap<String, Integer> students = new HashMap<>();
+        HashMap<Integer, String> students = new HashMap<>();
         Scanner in = new Scanner(System.in);
         String newStudent;
 
@@ -21,7 +21,7 @@ public class Second_HashMapStuents {
             if (!newStudent.equals("")) {
                 System.out.print("ID: ");
                 Integer newID = in.nextInt();
-                students.put(newStudent, newID);
+                students.put(newID, newStudent);
 
                 // Read in the newline before looping back
                 in.nextLine();
@@ -30,14 +30,14 @@ public class Second_HashMapStuents {
         } while(!newStudent.equals(""));
 
         // Print class roster
-        System.out.println("\nClass roster:");
+        System.out.println("\nClass Roster:");
 
-        String name = "jesse";
-        Integer jesseGrade = students.get(name);
-        for (Map.Entry<String, Integer> student : students.entrySet()) {
+//        String name = "jesse";
+//        Integer jesseGrade = students.get(name);
+        for (Map.Entry<Integer, String> student : students.entrySet()) {
             System.out.println(student.getKey() + " (" + student.getValue() + ")");
         }
-        System.out.println(jesseGrade);
+//        System.out.println(jesseGrade);
 
 
     }
